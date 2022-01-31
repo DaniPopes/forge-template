a450be462abf6fbaecb19ce2aed1fe7817372932
DaniPopes <contact@danipopes.com>
2022-01-31 01:12:06 +0000

    updated pragma, licenses, fixed test

--- README.md
+++ README.md
@@ -10,8 +10,7 @@ cd my-project
 forge init --template https://github.com/DaniPopes/forge-template
 
 ## initialize submodule dependencies
-forge update
-git submodule update --init --recursive 
+git submodule update --init --recursive
 
 ## install development dependencies
 yarn
@@ -21,7 +20,7 @@ yarn
 
 ### Testing Utilities
 
-Includes common testing contracts like `Hevm.sol` and `Console.sol`, as well as a `Utilities.sol` contract with common testing methods like creating users with an initial balance.
+Includes common testing contracts like `VM.sol` and `Console.sol`, as well as a `Utilities.sol` contract with common testing methods like creating users with an initial balance.
 
 ### Dependencies
 
@@ -32,18 +31,15 @@ Includes common testing contracts like `Hevm.sol` and `Console.sol`, as well as 
 Pre-configured `prettier` and `solhint`. Yarn is used for linting, make for everything else.
 
 ```bash
-## all are configured to run the solc optimizer with 100000 runs
 make build
 make test
-## test with verbosity 3
 make trace
 make clean
 make snapshot
-## runs yarn lint
+
 make lint
 
 yarn lint
-## doesn't write
 yarn lint:check
 ```
 
--- src/test/Contract.t.sol
+++ src/test/Contract.t.sol
@@ -1,29 +1,30 @@
-// SPDX-License-Identifier: Unlicense
-pragma solidity >=0.8.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
 
-import {DSTest} from "ds-test/test.sol";
-import {Utilities} from "./utils/Utilities.sol";
-import {console} from "./utils/Console.sol";
-import {Hevm} from "./utils/Hevm.sol";
+import { DSTest } from "ds-test/test.sol";
+import { Utilities } from "./utils/Utilities.sol";
+import { console } from "./utils/Console.sol";
+import { VM } from "./utils/VM.sol";
 
 contract ContractTest is DSTest {
-    Hevm internal immutable hevm = Hevm(HEVM_ADDRESS);
+	VM internal immutable vm = VM(HEVM_ADDRESS);
 
-    Utilities internal utils;
-    address payable[] internal users;
+	Utilities internal utils;
+	address payable[] internal users;
 
-    function setUp() public {
-        utils = new Utilities();
-        users = utils.createUsers(5);
-    }
+	function setUp() public {
+		utils = new Utilities();
+		users = utils.createUsers(5);
+	}
 
-    function testExample() public {
-        address payable alice = users[0];
-        console.log("alice's address", alice);
-        address payable bob = users[1];
-        hevm.prank(alice);
-        (bool sent, ) = bob.call{value: 10 ether}("");
-        assertTrue(sent);
-        assertGt(bob.balance, alice.balance);
-    }
+	function testExample() public {
+		address payable alice = users[0];
+		console.log("alice's address", alice);
+		address payable bob = users[1];
+		hevm.prank(alice);
+		// solhint-disable-next-line avoid-low-level-calls
+		(bool sent, ) = bob.call{ value: 10 ether }("");
+		assertTrue(sent);
+		assertGt(bob.balance, alice.balance);
+	}
 }
