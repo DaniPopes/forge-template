42f6f3aa95106d8821b40e4c02673d53129ab82c
DaniPopes <dani@danipopes.com>
2022-06-18 18:06:02 +0000

    add forge scripts

---
+++ src/scripts/Contract.s.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "forge-std/Script.sol";
+
+import { Contract } from "../Contract.sol";
+
+contract ContractScript is Script {
+    Contract public testContract;
+
+    function run() public {
+        vm.broadcast();
+        testContract = new Contract();
+    }
+}
